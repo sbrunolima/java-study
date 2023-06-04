1bb31a42ec0dd4868d90c3f12817973aaa5c0393
sbrunolima <bruno4study@outlook.com>
2023-06-03 23:10:15 -0300

    first commit

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,20 @@
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello, Tim");
+
+        boolean isAlien = false;
+        if (isAlien == false){
+            System.out.println("It is not an alien");
+        }
+
+        int topScore = 100;
+        if (topScore == 100){
+            System.out.println("You got the high score!");
+        }
+
+        int secondTopScore = 60;
+        if (topScore > secondTopScore && topScore < 100){
+            System.out.println("Greater than second top score and less tha 100");
+        }
+    }
+}
