b6bc7dd7dfada7c3f90e9078de6c3c0a09f19cf6
sbrunolima <bruno4study@outook.com>
2023-06-05 23:00:21 -0300

    update

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -20,5 +20,29 @@ public class Hello {
         if ((topScore > 90) || (secondTopScore <= 90)){
             System.out.println("Either or both of the conditions are true");
         }
+
+        String makeOfCar = "Volkswagen";
+        boolean isDomestic = makeOfCar == "Volkswagen" ? false : true;
+
+        if (isDomestic){
+            System.out.println("This car is domestic at our country");
+        }
+
+        String s = (isDomestic) ? "This car is domestic" : "This car is not domestic";
+
+        System.out.println(s);
+
+        double myFirstValue = 20.00d;
+        double mySecondValue = 80.00d;
+        double myValuesTotal = (myFirstValue + mySecondValue) * 100.00d;
+        System.out.println("MyValuesTotal = " + myValuesTotal);
+        double theReminder = myValuesTotal % 40.00d;
+        System.out.println("theReminder = " + theReminder);
+        boolean isNoRemainder = (theReminder == 0) ? true : false;
+        System.out.println("isNoRemainder = " + isNoRemainder);
+        if (!isNoRemainder){
+            System.out.println("Got some remainder");
+        }
+
     }
 }
