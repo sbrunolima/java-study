da7627c7f2db859f9cd21e93ede678abc001e3e4
sbrunolima <bruno4study@outlook.com>
2023-06-08 21:14:51 -0300

    update

---
+++ HelloWorld/src/MainChalllenge.java
@@ -0,0 +1,16 @@
+public class MainChalllenge {
+    public static void main(String[] args){
+        calculateScore(true, 800, 5, 100);
+        calculateScore(true, 10000, 8, 200);
+    }
+
+    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
+
+        int finalScore = score;
+
+        if (gameOver == true){
+            finalScore += (levelCompleted * bonus);
+            System.out.println("Your final score was " + finalScore);
+        }
+    }
+}
